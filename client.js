6961960ddfa7195b2ce45196d9f936679d0386ba
Glitch (promise-simple) <none>
2019-01-07 05:23:00 +0000

    added link for gitter chat

--- client.js
+++ client.js
@@ -283,7 +283,7 @@ function getGitInfoForUserAndDisplay(name) {
     */
     // helper function
     function addUserDetails(name, user) {
-        //     console.log(user);
+             console.log(user);
         let img = document.createElement('img');
         img.src = user.avatar_url;
         img.title = name + " == " + user.name + " == " + user.login;
@@ -292,12 +292,18 @@ function getGitInfoForUserAndDisplay(name) {
         figure.class = "figure";
         let figcaption = document.createElement('figcaption');
         figcaption.class = "figure-caption";
+
         //figcaption.textContent = name + ", " + user.name + ",";
-        figcaption.textContent = user.name + ",";
-        
+        figcaption.textContent = user.name + "@";
+        let gurl = document.createElement('a'); 
+        gurl.setAttribute("href", "https://gitter.im/" + name);
+        gurl.textContent = name + " "; 
+        figcaption.append(gurl);
+      
         let url = document.createElement('a'); 
         url.setAttribute("href", user.html_url);
-        url.textContent =  user.html_url.split('/').pop();
+        //url.textContent =  user.html_url.split('/').pop();
+        url.textContent =  "github";
         figcaption.append(url);
         
         figure.append(img);
